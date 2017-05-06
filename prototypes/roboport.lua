a4fdf419cadc8fea9d8715f4d9a8de8c5c5d333a
Fabian "Graveeater" Kürten <graveeater@sitl.de>
2017-05-06 16:52:27 +0200

    rewrite roboport update block into a function

--- prototypes/roboport.lua
+++ prototypes/roboport.lua
@@ -2,20 +2,33 @@ local round = function(x)
   return math.floor(x+0.5)
 end
 
-local roboport = data.raw["roboport"]["roboport"]
-local radiusRatio = 16 / roboport.logistics_radius
-local extraConstructionRadius = roboport.construction_radius - 2 * roboport.logistics_radius -- 0.15.9: 55 - 2 * 25 == 5
-local radiusRatioSquared = radiusRatio * radiusRatio
-roboport.logistics_radius = 16
-roboport.construction_radius = 2 * roboport.logistics_radius + extraConstructionRadius
+local function rescaleRoboportEntity(entity, newLogisticRadius)
+  local radiusRatio = newLogisticRadius / entity.logistics_radius
+  local extraConstructionRadius = entity.construction_radius - 2 * entity.logistics_radius -- 0.15.9: 55 - 2 * 25 == 5
+  local radiusRatioSquared = radiusRatio * radiusRatio
+  entity.logistics_radius = newLogisticRadius
+  entity.construction_radius = 2 * newLogisticRadius + extraConstructionRadius
 
--- TODO use the prototype value
-local oldEnergyUsage = tonumber(roboport.energy_usage:sub(1,-3))
-local oldUnit = roboport.energy_usage:sub(-2)
-roboport.energy_usage = round(oldEnergyUsage * radiusRatioSquared) .. oldUnit
+  local oldEnergyUsage = entity.energy_usage
+  local oldEnergyUsageNumber = tonumber(oldEnergyUsage:sub(1,-3))
+  local oldUnit = oldEnergyUsage:sub(-2)
+  entity.energy_usage = round(oldEnergyUsageNumber * radiusRatioSquared) .. oldUnit
 
-local recipe = data.raw["recipe"]["roboport"]
-local ingredients = recipe.ingredients
-for _,entry in ipairs(ingredients) do
-  entry[2] = round(entry[2] * radiusRatioSquared)
+  return radiusRatioSquared
 end
+
+local function rescaleRoboportRecipe(recipe, radiusRatioSquared)
+  local ingredients = recipe.ingredients
+  for _,entry in ipairs(ingredients) do
+    entry[2] = math.max(round(entry[2] * radiusRatioSquared), 1)
+  end
+end
+
+local function rescaleRoboport(name, newLogisticRadius)
+  local entity = data.raw["roboport"][name]
+  local radiusRatioSquared = rescaleRoboportEntity(entity, newLogisticRadius)
+  local recipe = data.raw["recipe"][name]
+  rescaleRoboportRecipe(recipe, radiusRatioSquared)
+end
+
+rescaleRoboport("roboport", 16)

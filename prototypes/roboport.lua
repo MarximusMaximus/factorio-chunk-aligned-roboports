9a70f3d40781d93d0d2d99f591852823d7c6aa53
Fabian "Graveeater" Kürten <graveeater@sitl.de>
2017-05-06 15:52:27 +0200

    update version, adapt roboport dimension to changed vanilla behaviour

--- prototypes/roboport.lua
+++ prototypes/roboport.lua
@@ -4,9 +4,10 @@ end
 
 local roboport = data.raw["roboport"]["roboport"]
 local radiusRatio = 16 / roboport.logistics_radius
+local extraConstructionRadius = roboport.construction_radius - 2 * roboport.logistics_radius -- 0.15.9: 55 - 2 * 25 == 5
 local radiusRatioSquared = radiusRatio * radiusRatio
 roboport.logistics_radius = 16
-roboport.construction_radius = 32 + 3
+roboport.construction_radius = 2 * roboport.logistics_radius + extraConstructionRadius
 
 -- TODO use the prototype value
 roboport.energy_usage = round(50 * radiusRatioSquared) .. "kW"

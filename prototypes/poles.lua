a12cc61d0bd59dbe0cb0ac6b14451a4bc84a374c
Fabian "Graveeater" Kürten <graveeater@sitl.de>
2021-02-03 20:36:17 +0100

    update for 1.1, add support for bobpower

--- prototypes/poles.lua
+++ prototypes/poles.lua
@@ -1,28 +1,38 @@
-local rescaleRecipe = require("prototypes/util")
+local rescaleRecipe = require('prototypes/util')
 
 local round = function(x)
   return math.floor(x+0.5)
 end
 
--- big pole
-do
-  local bigPole = data.raw["electric-pole"]["big-electric-pole"]
-  local radiusRatio = 32 / bigPole.maximum_wire_distance
-  local radiusRatioSquared = radiusRatio * radiusRatio
-  bigPole.maximum_wire_distance = 32
-
-  local recipe = data.raw["recipe"]["big-electric-pole"]
-  rescaleRecipe(recipe, radiusRatioSquared)
+local function rescalePoleEntity(entity, newReach, newSupplyRadius)
+  local reachRatio = newReach / entity.maximum_wire_distance
+  local supplyRatio = newSupplyRadius / entity.supply_area_distance
+  entity.maximum_wire_distance = newReach
+  entity.supply_area_distance = newSupplyRadius
+  return reachRatio * supplyRatio
 end
+
+local function rescalePole(name, newReach, newSupplyRadius)
+  local entity = data.raw['electric-pole'][name]
+  local costFactor = rescalePoleEntity(entity, newReach, newSupplyRadius)
+  local recipe = data.raw['recipe'][name]
+  rescaleRecipe(recipe, costFactor)
+end
+
+local function rescaleIfFound(name, newReach, newSupplyRadius)
+  if data.raw['electric-pole'][name] then
+    rescalePole(name, newReach, newSupplyRadius)
+  end
+end
+
+-- big pole
+rescaleIfFound('big-electric-pole', 32, 2)   -- 4 needed for 4 chunks
+rescaleIfFound('big-electric-pole-2', 43, 2) -- 3 needed for 4 chunks
+rescaleIfFound('big-electric-pole-3', 48, 2) -- 2 needed for 3 chunks
+rescaleIfFound('big-electric-pole-4', 64, 2) -- 2 needed for 4 chunks
 
 -- substation
-do
-  local substation = data.raw["electric-pole"]["substation"]
-  local radiusRatio = 8 / substation.supply_area_distance
-  local radiusRatioSquared = radiusRatio * radiusRatio  
-  substation.maximum_wire_distance = 16 + 2
-  substation.supply_area_distance = 8
-  
-  local recipe = data.raw["recipe"]["substation"]
-  rescaleRecipe(recipe, radiusRatioSquared)
-end
+rescaleIfFound('substation', 16, 16)
+rescaleIfFound('substation-2', 22, 22)
+rescaleIfFound('substation-3', 24, 24)
+rescaleIfFound('substation-4', 32, 32)
